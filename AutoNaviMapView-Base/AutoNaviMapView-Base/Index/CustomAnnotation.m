9c2adca23b35f911bac9184d673827ff8654be42
dingping <dingping@dingpings-iMac.local>
2017-03-06 17:13:59 +0800

    1.调整项目结构 2. 使用cocoapods管理第三方。

---
+++ AutoNaviMapView-Base/AutoNaviMapView-Base/Index/CustomAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  CustomAnnotation.m
+//  高德地图
+//
+//  Created by dingping on 2017/2/10.
+//  Copyright © 2017年 dingping. All rights reserved.
+//
+
+#import "CustomAnnotation.h"
+
+@implementation CustomAnnotation
+
+@end
---
+++ AutoNaviMapView-Base/AutoNaviMapView-Base/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoNaviMapView-Base
+//
+//  Created by dingping on 2017/2/13.
+//  Copyright © 2017年 dingping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
